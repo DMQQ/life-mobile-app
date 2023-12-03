311e34c580483fc82dd92d2a267fead1e5871dc6
dmqq <kozakdamian73@gmail.com>
2023-12-03 16:03:56 +0100

    feat: re-styled home widgets

--- components/HomeScreenWidgets/TodaysTimelinEvents.tsx
+++ components/HomeScreenWidgets/TodaysTimelinEvents.tsx
@@ -1,82 +1,99 @@
-import { FlatList, Text, View } from "react-native";
-import useUser from "../../utils/hooks/useUser";
-import { gql, useQuery } from "@apollo/client";
+import { StyleSheet, Text, View } from "react-native";
 import TimelineItem from "../../screens/Stack/Timeline/components/TimelineItem";
-import Colors, { Sizing } from "../../constants/Colors";
+import Colors, { Sizing, randColor } from "../../constants/Colors";
 import Color from "color";
-import Button from "../ui/Button/Button";
 import { useNavigation } from "@react-navigation/native";
 import Ripple from "react-native-material-ripple";
 import moment from "moment";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Padding, Rounded } from "../../constants/Layout";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: Padding.xxl,
+    marginTop: 15,
+    backgroundColor: "#00C896",
+    borderRadius: Rounded.xxl,
+  },
+
+  headContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+
+  heading: {
+    color: "#fff",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  button: {
+    backgroundColor: "#fff",
+    borderRadius: 100,
+    padding: 5,
+    paddingHorizontal: 10,
+  },
+  buttonText: {
+    color: Colors.primary,
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+
+  notFoundContainer: {
+    padding: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  notFoundText: {
+    fontSize: 18,
+    padding: 10,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
 
 export default function TodaysTimelineEvents(props: { data: any[] }) {
-  const { removeUser, token } = useUser();
-
   const navigation = useNavigation<any>();
 
+  const date = moment();
+
   return (
-    <View
-      style={{
-        padding: 15,
-        marginTop: 15,
-        backgroundColor: Color(Colors.primary).lighten(0.5).string(),
-        borderRadius: 10,
-      }}
-    >
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: 15,
-        }}
-      >
-        <Text
-          style={{
-            color: Colors.secondary,
-            fontSize: Sizing.heading,
-            fontWeight: "bold",
-          }}
-        >
-          Events {moment().format("MM.DD")}
+    <View style={styles.container}>
+      <View style={styles.headContainer}>
+        <Text style={styles.heading}>
+          {moment.weekdays()[date.day()]} {date.format("MM.DD")}
         </Text>
         <Ripple
           onPress={() => navigation.navigate("TimelineScreens")}
-          style={{
-            backgroundColor: Colors.secondary,
-            borderRadius: 100,
-            padding: 5,
-            paddingHorizontal: 10,
-          }}
+          style={styles.button}
         >
-          <Text
-            style={{
-              color: Colors.primary,
-              fontSize: 15,
-              fontWeight: "bold",
-            }}
-          >
-            See more
-          </Text>
+          <Text style={styles.buttonText}>See more</Text>
         </Ripple>
       </View>
 
-      {props?.data?.map((timeline) => (
-        <TimelineItem location="root" key={timeline.id} {...timeline} />
+      {props?.data?.slice(0, 3).map((timeline) => (
+        <TimelineItem
+          styles={{
+            backgroundColor: "#ffffff25",
+            borderRadius: 15,
+            paddingHorizontal: 20,
+          }}
+          textColor={"#fff"}
+          location="root"
+          key={timeline.id}
+          {...timeline}
+        />
       ))}
 
       {props?.data?.length === 0 && (
-        <Text
-          style={{
-            paddingVertical: 5,
-            color: "#fff",
-            fontSize: 20,
-            fontWeight: "bold",
-            textAlign: "center",
-          }}
-        >
-          No events today
-        </Text>
+        <View style={styles.notFoundContainer}>
+          <MaterialIcons name="event-busy" size={35} color={"#fff"} />
+
+          <Text style={styles.notFoundText}>No events</Text>
+        </View>
       )}
     </View>
   );
