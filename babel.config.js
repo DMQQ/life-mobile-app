fcec3c2a4df70d353abca32f4724d22a8cacb4fe
dmqq <kozakdamian73@gmail.com>
2023-12-21 14:49:54 +0100

    feat: Added path alias

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,30 @@ module.exports = function (api) {
       "babel-preset-expo",
       //  "module:metro-react-native-babel-preset"
     ],
-    plugins: ["react-native-reanimated/plugin", ["module-resolver"]],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          extensions: [
+            ".ios.js",
+            ".android.js",
+            ".ios.jsx",
+            ".android.jsx",
+            ".js",
+            ".jsx",
+            ".json",
+            ".ts",
+            ".tsx",
+          ],
+          root: ["."],
+          alias: {
+            "@": "./",
+
+            // "@components":"./components" etc
+          },
+        },
+      ],
+    ],
   };
 };
