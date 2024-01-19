966b778b4c7363c303c09365361fdc45792b3f8b
DMQQ <kozakdamian73@gmail.com>
2024-01-19 16:51:42 +0100

    feat: Added timeline editing and refactored timeline screens

--- screens/Stack/Timeline/types.ts
+++ screens/Stack/Timeline/types.ts
@@ -3,7 +3,11 @@ import { StackScreenProps } from "../../../types";
 export type TimelineRootStack = {
   Timeline: undefined;
   TimelineDetails: { timelineId: string };
-  TimelineCreate: { selectedDate: string };
+  TimelineCreate: {
+    selectedDate: string;
+    mode: "create" | "edit";
+    timelineId?: string;
+  };
   ImagesPreview: { selectedImage: string; timelineId: string };
   Schedule: { selected: string };
 };
