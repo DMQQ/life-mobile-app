b8e0fdb4816b1dbc4116d5e10ba0d3dc3ada004f
Damian Derlecki <dmq@Damians-MacBook-Pro.local>
2024-08-05 21:26:05 +0200

    Feat: improve filtering options and integrate with gql api

--- screens/Stack/Wallet/hooks/useGetWallet.ts
+++ screens/Stack/Wallet/hooks/useGetWallet.ts
@@ -31,6 +31,8 @@ const init = {
   },
 
   category: [] as string[],
+
+  type: undefined as string | undefined,
 };
 
 export type Filters = typeof init;
@@ -41,7 +43,9 @@ export type Action =
   | { type: "SET_AMOUNT_MAX"; payload: number }
   | { type: "SET_DATE_MIN"; payload: string }
   | { type: "SET_DATE_MAX"; payload: string }
-  | { type: "SET_CATEGORY"; payload: string[] };
+  | { type: "SET_CATEGORY"; payload: string[] }
+  | { type: "SET_TYPE"; payload: string | undefined }
+  | { type: "TOGGLE_CATEGORY"; payload: string };
 
 const reducer = (state: typeof init, action: Action) => {
   if (action.type === "SET_QUERY") {
@@ -90,6 +94,26 @@ const reducer = (state: typeof init, action: Action) => {
       category: action.payload,
     };
   }
+  if (action.type === "TOGGLE_CATEGORY") {
+    const index = state.category.indexOf(action.payload);
+    if (index === -1) {
+      return {
+        ...state,
+        category: [...state.category, action.payload],
+      };
+    } else if (index > -1) {
+      return {
+        ...state,
+        category: state.category.filter((c) => c !== action.payload),
+      };
+    }
+  }
+  if (action.type === "SET_TYPE") {
+    return {
+      ...state,
+      type: action.payload,
+    };
+  }
 
   return state;
 };
@@ -115,9 +139,12 @@ export default function useGetWallet() {
             to: filters.date.to,
           },
           category: filters.category,
+          ...(filters.type && { type: filters.type }),
         },
       });
     }, 1000);
+
+    console.log("filters", filters);
 
     return () => clearTimeout(timeout);
   }, [filters]);
