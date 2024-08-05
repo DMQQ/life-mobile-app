0b3c2f4d8e96d8ec364d09925a6937be70ff34df
DMQQ <kozakdamian73@gmail.com>
2024-08-05 21:29:24 +0200

    Install universal platform time/date picker & add safe areas

--- constants/Url.ts
+++ constants/Url.ts
@@ -1,6 +1,6 @@
 const isDev = false;
 
-const API = isDev ? "http://192.168.0.25:3000" : "http://srv25.mikr.us:40034";
+const API = isDev ? "http://192.168.0.31:3000" : "http://srv25.mikr.us:40034";
 
 export default {
   API: API, // "http://srv11.mikr.us:30220", // "http://192.168.0.25:3000"
