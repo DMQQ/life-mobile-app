a86f8c0ccb058bd3c60b3af7ebb406dae2ef6b19
DMQQ <kozakdamian73@gmail.com>
2024-08-11 14:40:35 +0200

    Feat: moved action buttons to header

---
+++ components/ui/Header/Header.tsx
@@ -0,0 +1,64 @@
+import Animated, { AnimatedStyle } from "react-native-reanimated";
+import IconButton from "../IconButton/IconButton";
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { View } from "react-native";
+
+export default function Header(props: {
+  buttons: {
+    onPress: () => void;
+    icon: JSX.Element | React.ReactNode;
+  }[];
+  title?: string;
+  goBack?: boolean;
+  titleAnimatedStyle?: AnimatedStyle;
+}) {
+  const navigation = useNavigation();
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        padding: 10,
+        paddingHorizontal: 15,
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      {props.goBack && (
+        <IconButton
+          onPress={() => navigation.canGoBack() && navigation.goBack()}
+          icon={<AntDesign name="arrowleft" size={24} color="#fff" />}
+        />
+      )}
+
+      {props.titleAnimatedStyle && props.title && (
+        <Animated.Text
+          style={[
+            {
+              color: "#fff",
+              fontSize: 16,
+              fontWeight: "600",
+              letterSpacing: 0.5,
+            },
+            props.titleAnimatedStyle,
+          ]}
+        >
+          {props.title}
+        </Animated.Text>
+      )}
+
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "flex-end",
+          flexDirection: "row",
+          gap: 5,
+        }}
+      >
+        {props.buttons.map((button, index) => (
+          <IconButton key={index} onPress={button.onPress} icon={button.icon} />
+        ))}
+      </View>
+    </View>
+  );
+}
