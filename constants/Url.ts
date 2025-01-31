4ff76957910964b79ed313055def51db64204c51
DMQQ <kozakdamian73@gmail.com>
2025-01-31 22:12:22 +0100

    remove react-native-shared-element (caused error on build), changed backend url

--- constants/Url.ts
+++ constants/Url.ts
@@ -1,6 +1,6 @@
 const isDev = false;
 
-const API = isDev ? "http://192.168.0.25:3001" : "http://srv25.mikr.us:40034";
+const API = isDev ? "http://192.168.0.25:3001" : "https://life.bieda.it";
 
 export default {
   API: API, // "http://srv11.mikr.us:30220", // "http://192.168.0.25:3000"
